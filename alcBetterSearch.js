51251018c4adb3178c1b9d306f5ab39be80019c4
francoiskielbasa <francois@doyoubuzz.com>
2015-10-27 19:11:33 +0100

    First Commit

---
+++ alcBetterSearch.js
@@ -0,0 +1,90 @@
+/**
+ * Created by francois on 27/10/15.
+ */
+function getTitle(movieElement) {
+    return movieElement.querySelector('.titlebar').textContent.trim();
+}
+
+function getTimes(movieElement) {
+    if (typeof movieElement.querySelector(".showtimescore .last") !== 'undefined' && movieElement.querySelector(".showtimescore .last").textContent == "Aucune séance pour l'horaire sélectionné ") {
+        return null;
+    }
+    var times = [];
+    var timesElement = getTimesElements(movieElement);
+    for (var i = 0; i < timesElement.length; i++) {
+        var time = {datetime: getTime(timesElement[i]), isActive: isActive(timesElement[i])};
+        times.push(time);
+    }
+    return times;
+}
+
+function getTime(timesElement) {
+    time = timesElement.getAttribute('data-times');
+    var regex = /(\[)"(\d{2}:\d{2})(.*)/;
+    var m;
+
+    if ((m = regex.exec(time)) !== null) {
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++;
+        }
+    }
+    return m[2];
+}
+
+function isActive(timesElement) {
+    return !timesElement.classList.contains('btn-disabled');
+}
+
+function getMovies() {
+    var moviesElement = document.querySelector('.colgeneral').querySelectorAll(".datablock");
+    var movies = [];
+    for (var i = 0; i < moviesElement.length; i++) {
+        var movie = {element: moviesElement[i], title: getTitle(moviesElement[i]), time: getTimes(moviesElement[i])};
+        movies.push(movie);
+    }
+    return movies;
+}
+
+function getTimesElements(movieElement) {
+    return movieElement.getElementsByTagName("em");
+}
+
+function isDisplaying(movie, value) {
+    if (movie.time === null) {
+        return false;
+    }
+    for (var i = 0; i < movie.time.length; i++) {
+        if (Date.parse('01/01/2001 ' + movie.time[i].datetime) > Date.parse('01/01/2001 ' + value)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+var date = '22:00';
+var movies = getMovies();
+movies.forEach(function (movie) {
+    if (!isDisplaying(movie, date)) {
+        movie.element.style.display = 'none';
+    }
+
+});
+
+/*
+ for (var i = 0; i < movies.length; i++) {
+ if (countShowing(movies[i]) == 0) {
+ movies[i].style.display = "none";
+ }
+ */
+
+
+
+//var title = movies[i].querySelector(".titlebar").textContent;
+//var hour = movies[i].querySelector(".showtimescore .times").textContent;
+//alert(title + hour);
+/*
+ if ( hour === "Aucune séance pour l'horaire selectionné") {
+ movies[i].style.display = "none";
+ }
+ */
+
