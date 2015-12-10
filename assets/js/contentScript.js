9e1a1962ecd0561ddf8d24295053109ace7a2a0d
francoiskielbasa <francois.kielbasa@gmail.com>
2015-12-10 20:24:46 +0100

    V2 : big refactoring and time filters added

--- assets/js/contentScript.js
+++ assets/js/contentScript.js
@@ -1,103 +1,169 @@
 /**
  * Created by francois on 27/10/15.
  */
-function getTimes(movieElement) {
-    if (typeof movieElement.querySelector(".showtimescore .last") !== 'undefined' && movieElement.querySelector(".showtimescore .last").textContent == "Aucune séance pour l'horaire sélectionné ") {
-        return null;
-    }
-    var times = [];
-    var timesElement = getTimesElements(movieElement);
-    for (var i = 0; i < timesElement.length; i++) {
-        var time = {datetime: getTime(timesElement[i]), isActive: isActive(timesElement[i])};
-        times.push(time);
-    }
-    return times;
+
+/**
+ * Class Filter
+ */
+function Filter(isActive, hideUnShowed, startTimeFrom, startTimeTo) {
+    this.isActive = isActive;
+    this.hideUnShowed = hideUnShowed;
+    this.startTimeFrom = startTimeFrom;
+    this.startTimeTo = startTimeTo;
 }
 
-function getTime(timesElement) {
-    var time = timesElement.getAttribute('data-times');
-    var regex = /(\[)"(\d{2}:\d{2})(.*)/;
-    var m;
+/**
+ * Class Time
+ */
+function Time(timeElement) {
+    var getTime = function (timeElement) {
+        var time = timeElement.getAttribute('data-times');
+        var regex = /(\[)"(\d{2}:\d{2})(.*)/;
+        var m;
 
-    if ((m = regex.exec(time)) !== null) {
-        if (m.index === regex.lastIndex) {
-            regex.lastIndex++;
+        if ((m = regex.exec(time)) !== null) {
+            if (m.index === regex.lastIndex) {
+                regex.lastIndex++;
+            }
         }
+        return m[2];
+    };
+
+    this.dateTime = getTime(timeElement);
+    this.isActive = function (timeElement) {
+        return !timeElement.classList.contains('btn-disabled');
+    };
+}
+
+/**
+ * Class Movie
+ */
+function Movie(DOMElement) {
+    var getTimesElements = function (DOMElement) {
+        var pan = DOMElement.getElementsByClassName("pane");
+        var index;
+
+        for (var i = 0; i < pan.length; i++) {
+            if (!pan[i].classList.contains('hide')) {
+                index = i;
+            }
+        }
+
+        return pan[index].getElementsByTagName("em");
+    };
+
+    var getTimes = function (DOMElement) {
+        if (typeof DOMElement.querySelector(".showtimescore .last") !== 'undefined' && DOMElement.querySelector(".showtimescore .last").textContent == "Aucune séance pour l'horaire sélectionné ") {
+            return null;
+        }
+        var times = [];
+        var timesElement = getTimesElements(DOMElement);
+        for (var i = 0; i < timesElement.length; i++) {
+            var time = new Time(timesElement[i]);
+            times.push(time);
+        }
+        return times;
+    };
+
+    var testIfIsShowedToday = function (timesArray) {
+        for (var i = 0; i < timesArray.length; i++) {
+            if (timesArray[i].isActive) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    this.DOMElement = DOMElement;
+    this.timesArray = getTimes(DOMElement);
+    this.isShowedToday = testIfIsShowedToday(this.timesArray);
+}
+
+Movie.prototype.shouldBeDisplayed = function (filter) {
+    var from, to;
+
+    if (!filter.isActive) {
+        return true;
     }
-    return m[2];
-}
 
-function isActive(timesElement) {
-    return !timesElement.classList.contains('btn-disabled');
-}
+    if (filter.hideUnShowed && !this.isShowedToday) {
+        return false;
+    }
 
+    //if (filter.startTimeFrom === '') {
+    //    from = '00:00';
+    //} else {
+    from = filter.startTimeFrom;
+    //}
+
+    //if (filter.startTimeTo === '') {
+    //    to = '23:59';
+    //} else {
+    to = filter.startTimeTo;
+    //}
+
+    if (from !== '' || to !== '') {
+        if (!this.isShowedToday) {
+            return true;
+        }
+        if (from === '') {
+            from = '00:00';
+        }
+        if (to === '') {
+            to = '23:59';
+        }
+        for (var i = 0; i < this.timesArray.length; i++) {
+            if (this.timesArray[i].dateTime >= from && this.timesArray[i].dateTime <= to) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    return true;
+};
+
+/**
+ * Function getMovies
+ * return an array of movies
+ */
 function getMovies() {
     var moviesElement = document.querySelector('.colgeneral').querySelectorAll(".datablock");
     var movies = [];
     for (var i = 0; i < moviesElement.length; i++) {
-        var movie = {element: moviesElement[i], time: getTimes(moviesElement[i])};
-        movies.push(movie);
+        movies.push(new Movie(moviesElement[i]));
     }
     return movies;
 }
 
-function getTimesElements(movieElement) {
-    var pan = movieElement.getElementsByClassName("pane");
-    var index;
+/**
+ * Function run
+ * Main process is here
+ */
+function run(movies) {
+    chrome.storage.local.get(['filtersAreActive', 'hideUnShowed', 'startTimeFrom', 'startTimeTo'], function (result) {
+        var filter = new Filter(result.filtersAreActive, result.hideUnShowed, result.startTimeFrom, result.startTimeTo);
+        movies.forEach(function (movie) {
+            if (movie.shouldBeDisplayed(filter)) {
+                movie.DOMElement.style.display = 'block';
+            } else {
+                movie.DOMElement.style.display = 'none';
+            }
+        });
 
-    for (var i = 0; i < pan.length; i++) {
-        if (!pan[i].classList.contains('hide')) {
-            index = i;
-        }
-    }
-
-    return pan[index].getElementsByTagName("em");
-}
-
-function isDisplaying(movie) {
-    if (movie.time === null) {
-        return false;
-    }
-    for (var i = 0; i < movie.time.length; i++) {
-        if (movie.time[i].isActive) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function displayNone(movies, date) {
-    movies.forEach(function (movie) {
-        if (!isDisplaying(movie, date)) {
-            movie.element.style.display = 'none';
-        }
     });
 }
 
-function displayAll(movies) {
-    movies.forEach(function (movie) {
-        movie.element.style.display = 'block';
-    })
-}
-
-function run() {
-    var movies = getMovies();
-    chrome.storage.local.get('unShowed', function (result) {
-        var hide = result.unShowed;
-        if (hide) {
-            displayNone(movies);
-        } else {
-            displayAll(movies);
-        }
-    });
-}
-
+/**
+ * Plugin update listener
+ */
 chrome.runtime.onMessage.addListener(
     function (request) {
         if (request.reload) {
-            run();
+            run(movies);
         }
     }
 );
 
-run();
+var movies = getMovies();
+run(movies);
--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,39 +1,57 @@
 /**
  * Created by francois on 28/10/15.
  */
-function reload () {
-	chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
+var allFilters = document.getElementById('allFilters');
+var activeFilters = document.getElementById('activeFilters');
+var displayUnShowedButton = document.getElementById('displayUnShowedButton');
+var fromButton = document.getElementById('fromButton');
+var toButton = document.getElementById('toButton');
+
+function reload() {
+    if (activeFilters.checked) {
+        allFilters.style.display = 'block';
+    } else {
+        allFilters.style.display = 'none';
+    }
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT}, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {reload: true});
     })
 }
 
-var unShowedButton = document.getElementById('unShowedButton');
-var fromButton = document.getElementById('fromButton');
-var toButton = document.getElementById('toButton');
-
 document.addEventListener('DOMContentLoaded', function () {
-    chrome.storage.local.get(['unShowed', 'timeFrom', 'timeTo'], function (result) {
-        unShowedButton.checked = result.unShowed;
-        fromButton.value = result.timeFrom;
-        toButton.value = result.timeTo;
+    chrome.storage.local.get(['filtersAreActive', 'hideUnShowed', 'startTimeFrom', 'startTimeTo'], function (result) {
+        displayUnShowedButton.checked = result.hideUnShowed;
+        fromButton.value = result.startTimeFrom;
+        toButton.value = result.startTimeTo;
+        activeFilters.checked = result.filtersAreActive;
+        reload();
     });
 });
 
-unShowedButton.addEventListener('click', function () {
-    chrome.storage.local.set({'unShowed': this.checked});
+activeFilters.addEventListener('click', function () {
+    chrome.storage.local.set({'filtersAreActive': this.checked});
     reload();
-    
+});
+
+displayUnShowedButton.addEventListener('click', function () {
+    chrome.storage.local.set({'hideUnShowed': this.checked});
+    reload();
 });
 
 fromButton.addEventListener('change', function () {
-	chrome.storage.local.set({'timeFrom' : this.value});
-	reload();
+    debugger;
+    chrome.storage.local.set({'startTimeFrom': this.value});
+    reload();
 });
 
 toButton.addEventListener('change', function () {
-	chrome.storage.local.set({'timeTo' : this.value});
-	if(this.valueAsDate<fromButton.valueAsDate) {
-		document.getElementById('error_message').style.display="block";
-	}
-	reload();
+    chrome.storage.local.set({'startTimeTo': this.value});
+    /*
+    if (this.valueAsDate < fromButton.valueAsDate) {
+        document.getElementById('error_message').style.display = "block";
+    } else {
+        document.getElementById('error_message').style.display = "none";
+    }
+    */
+    reload();
 });
